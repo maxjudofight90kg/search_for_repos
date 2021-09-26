b5a09ad1e273a84ccc6b746d735658e1ad72ee79
MacBook <macbook@MacBook-Pro-MacBook-3.local>
2021-09-26 12:40:05 +0300

    repos

---
+++ src/components/Alert.js
@@ -0,0 +1,18 @@
+import React, {useContext} from 'react'
+import { AlertContext } from '../context/alert/AlertContext'
+
+const Alert = () => {
+const {alert, hide} = useContext(AlertContext)
+    if (!alert) return null
+    return (
+        <div 
+            className={`alert alert-${alert.type || 'secondary'} alert-dismissible`} 
+            role="alert"
+        >
+        {alert.text}
+            <button type="button" className="btn-close" aria-label="Close" onClick={hide}></button>
+        </div>
+    )
+}
+
+export default Alert
